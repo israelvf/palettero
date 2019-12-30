b1a0c7ab5fcba1df4c8ecc38f108d5f2744eec37
Israel Ferreira <israelvf@gmail.com>
2019-12-30 20:53:43 -0300

    Fix color palette darkest color

--- src/theme.py
+++ src/theme.py
@@ -1,4 +1,5 @@
 import configparser
+from math import sqrt
 
 
 class Theme:
@@ -19,10 +20,27 @@ class Theme:
     def hex_to_rgb(self, palette):
         rgb_list = []
         for color in palette:
-            rgb_list = rgb_list + [int(color[i:i+2], 16) for i in (0, 2, 4)]
+            rgb_list += [int(color[i:i+2], 16) for i in (0, 2, 4)]
         return rgb_list
+
+    def find_darkest_color(self, palette):
+        rgb_colors = []
+        rgb_color = []
+        module = []
+        for channel in palette:
+            rgb_color += [channel]
+            if len(rgb_color) == 3:
+                rgb_colors += [rgb_color]
+                rgb_color = []
+        if rgb_color:
+            rgb_colors += [rgb_color]
+        for color in rgb_colors:
+            module += [sqrt(color[0]**2 + color[1]**2 + color[2]**2)]
+        darkest_index = module.index(min(module))
+        return rgb_colors[darkest_index]
 
     def format_palette(self, palette):
         if len(palette) < 256:
-            return palette + [0, ] * (256-len(palette)) * 3
+            darkest_color = self.find_darkest_color(palette)
+            return palette + darkest_color * (256-len(palette))
         return palette[:255]
