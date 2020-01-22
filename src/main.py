4fc07736b646e6c577f916d1cf122e9e063e96fb
Israel Ferreira <israelvf@gmail.com>
2020-01-22 14:54:59 -0300

    Some refactoring in file loading

--- src/main.py
+++ src/main.py
@@ -21,7 +21,6 @@ optional.add_argument(
     "-g", "--glitch", type=int, help="add color glitch to image")
 
 if __name__ == "__main__":
-    kernel_size = 0
     args = parser.parse_args()
 
     main_dir, main_file = os.path.split(__file__)
@@ -30,32 +29,27 @@ if __name__ == "__main__":
         parser.error("theme must be a file in themes directory")
     palette = Theme(theme).palette
 
-    if args.smooth:
-        if args.smooth % 2 == 0:
-            args.smooth += 1
-        kernel_size = args.smooth
-
     if os.path.isdir(args.input):
         directory = os.fsencode(args.input)
         for file in os.listdir(directory):
             filename = os.fsdecode(file)
-            if filename.lower().endswith(".jpg") or filename.lower().endswith(".png"):
+            if filename.lower().endswith((".png", ".jpg", ".jpeg", ".tiff", ".bmp", ".gif")):
                 file_path = os.path.join(args.input, filename)
                 output = args.output if args.output else os.path.join(
-                    args.input, filename[:-4] + "_edt" + filename[-4:])
+                    args.input, filename.split(".")[0] + "_edt." + filename.split(".")[1])
                 if os.path.isdir(output):
                     output = os.path.join(output, filename)
-                Image(file_path, palette, kernel_size,
+                Image(file_path, palette, args.smooth,
                       args.glitch).save(output)
             else:
                 continue
     elif os.path.isfile(args.input):
-        if args.input.lower().endswith(".jpg") or args.input.lower().endswith(".png"):
-            output = args.output if args.output else args.input[:-
-                                                                4] + "_edt" + args.input[-4:]
+        if args.input.lower().endswith((".png", ".jpg", ".jpeg", ".tiff", ".bmp", ".gif")):
+            output = args.output if args.output else args.input.split(
+                ".")[0] + "_edt." + args.input.split(".")[1]
             if os.path.isdir(output):
                 file_dir, filename = os.path.split(args.input)
                 output = os.path.join(output, filename)
-            Image(args.input, palette, kernel_size, args.glitch).save(output)
+            Image(args.input, palette, args.smooth, args.glitch).save(output)
     else:
         parser.error("input must be a valid file or a directory")
